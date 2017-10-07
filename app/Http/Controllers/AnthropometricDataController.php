03a474a9dacbe4322930e2b0ef43b261ade26012
Vitor Teofilo <vitor.teo1995@hotmail.com>
2017-10-07 14:30:46 -0300

    up

--- app/Http/Controllers/AnthropometricDataController.php
+++ app/Http/Controllers/AnthropometricDataController.php
@@ -11,9 +11,9 @@ class AnthropometricDataController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        dd('123');
     }
 
     /**
