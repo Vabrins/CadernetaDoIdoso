6fe2452bea4f4f03661a541a6bf0938a3629c26f
Vitor Teofilo <vitor.teo1995@hotmail.com>
2017-10-29 16:21:11 -0200

    of tenant

--- app/Http/Controllers/ElderlyController.php
+++ app/Http/Controllers/ElderlyController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use HipsterJazzbo\Landlord\Facades\Landlord;
 
-class ElderlyController extends Controller
+class ElderlyController extends ElderlyDataController
 {
-    //
+    public function show($identification){
+    	if(strlen($identification) < 11){
+    		$elderly = \App\Elderly::find($identification);
+    	}else{
+    		$elderly = \App\Elderly::where('document_cpf', $identification)->first();
+    		if(empty($elderly['id'])){
+    			return false;
+    		}
+    		$identification = $elderly['id'];
+    	}
+    	setcookie("id_elderly", $identification,0,"/");
+    	Landlord::addTenant('id_elderly', $identification);
+
+    	parent::show($identification);
+    }
 }
--- app/Http/Controllers/ElderlyDataController.php
+++ app/Http/Controllers/ElderlyDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use HipsterJazzbo\Landlord\Facades\Landlord;
+use Session;
 
 class ElderlyDataController extends Controller
 {
@@ -10,7 +11,9 @@ class ElderlyDataController extends Controller
     public function __construct(){
         $this->model = 'App\\'.str_replace('Controller', '', class_basename(get_called_class())
         );
-        Landlord::addTenant('id_elderly', 1);
+        if(!empty($_COOKIE['id_elderly'])){
+            Landlord::addTenant('id_elderly', $_COOKIE['id_elderly']);
+        }
     }
 
     /**
